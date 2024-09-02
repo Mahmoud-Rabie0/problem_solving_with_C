030e4892ba7f84965ba89570a33495b5ea24019d
Mahmoud Rabie <mahmoud.rabie.swe@gmail.com>
2024-09-02 15:05:58 +0300

    Some updates

---
+++ Sheet 1 (Level 1)/Amer and Triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int  a, b, c;
+    scanf("%d %d %d", &c, &a, &b);
+ 
+    if(a * a + b * b == c *c)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
